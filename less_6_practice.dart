d98b91d96280262c7fe28d9db024e18152d8867c
Господин <rdogdurbaev1@gmail.com>
2022-12-01 18:32:40 +0600

    first commit

---
+++ less_6_practice.dart
@@ -0,0 +1,225 @@
+import 'dart:io';
+
+void main() {
+  // #1 zadanie
+  // num a = 5;
+  // num b = 5.126;
+
+  // num slozhenie = a + b;
+  // num vychitanie = a - b;
+  // num umnozhenie = a * b;
+  // num delenie = a / b;
+  // print(slozhenie);
+  // print(vychitanie);
+  // print(umnozhenie);
+  // print(umnozhenie);
+  // print(delenie);
+
+  // #2 zadanie
+
+  // num a1 = 5;
+  // num a2 = 2;
+  // print("$a1 + $a2 = ${a1 + a2}");
+  // print("$a1 + $a2 = ${a1 - a2}");
+
+  // num b1 = 132;
+  // num b2 = 123;
+  // print("$b1 + $b2 = ${b1 * b2}");
+
+  // num c1 = 78;
+  // num c2 = 2;
+  // num c3 = 2.5;
+  // num c4 = c1 / c2;
+  // print("$c1 / $c2 = $c4");
+  // print("$c1 / $c3 = ${c1 / c3}");
+
+  // num f = 0;
+  // num f1 = 5;
+  // print("0 * 5 = ${f * f1}");
+
+  // #3 zadanie
+
+  // var arr = [5, 2];
+  // print(arr.reversed);
+
+  // #4 zadanie
+
+  //  stdout.write("Введите ваше имя: ");
+
+  // String name = stdin.readLineSync()!;
+
+  // stdout.write("Ввелите ваш возраст: ");
+  // int age = int.parse(stdin.readLineSync()!);
+  // print("Вас зовут $name и вам $age лет");
+
+  // #5 zadanie
+
+  // stdout.write("Ввелите ваш номер телефона: ");
+  // int age = int.parse(stdin.readLineSync()!);
+
+  // if (age < 5) {
+  //   print("не достаточно символов");
+  // }
+  // if (age == 6) {
+  //   print("Ваш номер телефона: $age");
+  // }
+  // } if(age > 15) {
+  //   print("2");
+  // }
+
+  // #7 zadanie
+
+  // bool isRain = true;
+  // isRain == true ? print("Берите зонт") : print("Не берите зонт");
+
+  // #8 zadanie
+
+  // num a1 = 4;
+
+  // if (a1 > 3) {
+  //   print(a1 + 10);
+  // } else {
+  //   print(a1 - 10);
+  // }
+
+  // #9 zadanie
+
+  // num a = 9;
+
+  // if (a < 7) {
+  //   print("yes");
+  // } if (a > 10) {
+  //   print("no");
+  // } if (a == 9) {
+  //   print("error");
+  // }
+
+  // #10 zadanie
+
+  // stdout.write("Число а ");
+  // int a = int.parse(stdin.readLineSync()!);
+
+  // stdout.write("Число б ");
+  // int b = int.parse(stdin.readLineSync()!);
+
+  // if (a > b) {
+  //   print(a);
+  // } else if (b > a) {
+  //   print(b);
+  // }
+
+  // #11 zadanie
+
+  // stdout.write("Число а ");
+  // int a = int.parse(stdin.readLineSync()!);
+
+  // stdout.write("Число б ");
+  // int b = int.parse(stdin.readLineSync()!);
+
+  // int c = a + b;
+
+  // if (c > 100) {
+  //   print("yes");
+  // } else if (c < 100) {
+  //   print("no");
+
+  // #12 zadanie
+
+  // num a = 10;
+  // num b = 15;
+  // num c = 20;
+
+  // if (a < b || a < c) {
+  //   print(a);
+  // } else if (b < a || b < c) {
+  //   print(b);
+  // } else if (c < a || c < b) {
+  //   print(c);
+  // }
+
+  // #13 zadanie
+
+  // stdout.write("Ввелите число: ");
+  // int age = int.parse(stdin.readLineSync()!);
+
+  // switch (age) {
+  //   case 1:
+  //     print("zima");
+  //     break;
+
+  //   case 2:
+  //     print("zima");
+  //     break;
+
+  //   case 3:
+  //     print("zima");
+  //     break;
+
+  //   case 4:
+  //     print("vesna");
+  //     break;
+
+  //   case 5:
+  //     print("vesna");
+  //     break;
+
+  //   case 6:
+  //     print("vesna");
+  //     break;
+
+  //   case 7:
+  //     print("leto");
+  //     break;
+
+  //   case 8:
+  //     print("leto");
+  //     break;
+
+  //   case 9:
+  //     print("zima");
+  //     break;
+
+  //   case 10:
+  //     print("zima");
+  //     break;
+
+  //   case 11:
+  //     print("zima");
+  //     break;
+
+  //   case 12:
+  //     print("zima");
+  //     break;
+
+  //   default:
+  //     print("такого числа не сузествует");
+  // }
+
+  // #14 zadanie
+
+  // List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
+  // if (a < 5){
+
+  // }
+
+  // #15 zadanie
+
+  // List arr = [];
+
+  // for (var i = 0; i < 5; i++) {
+  //   arr.add("*");
+  //   print(arr);
+  // }
+
+  // #16 zadanie
+
+  // for (var i = 0; i <= 5; i++) {
+  //   print(i);
+  // }
+
+  // #17 zadanie
+  // for (var i = 5; i >= 0; i--) {
+  //   print(i);
+  // }
+}
